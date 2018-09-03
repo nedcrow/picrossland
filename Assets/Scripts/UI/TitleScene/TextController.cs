1c6c679d12d7bdb02d16af8b93a30781fbdf2859
nedcorw <nedcrow0@gmail.com>
2018-09-03 19:26:42 +0900

    수정     - TitleScene / Title     - TitleScene / LoginText

---
+++ Assets/Scripts/UI/TitleScene/TextController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+    public int count = 3;
+
+	void Start () {
+        if (GetComponent<Text>()) { StartCoroutine(AddDot(GetComponent<Text>())); }
+	}
+	
+	IEnumerator AddDot(Text target)
+    {
+        string startText = target.text;
+        while (true)
+        {
+            for(int i=0; i<count; i++)
+            {
+                target.text = target.text + ".";
+                yield return new WaitForSeconds(0.5f);
+            }
+            target.text = startText;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
